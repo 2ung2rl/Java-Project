c6651c5ca2ec2624b6fcb9cdef57437890ea791e
2ung2rl <155498417+2ung2rl@users.noreply.github.com>
2024-02-05 02:30:01 +0900

    Add files via upload

---
+++ src/twoweeks/Grocery.java
@@ -0,0 +1,7 @@
+package twoweeks;
+
+public class Grocery extends Product {
+    Grocery(String name, int price, double weight) {
+        super(name, price, weight);
+    }
+}
---
+++ src/twoweeks/LargeAppliance.java
@@ -0,0 +1,7 @@
+package twoweeks;
+
+public class LargeAppliance extends Product {
+    LargeAppliance(String name, int price, double weight) {
+        super(name, price, weight);
+    }
+}
