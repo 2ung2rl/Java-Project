7720ca1131a8a47b5d78eb6c76bd8f3de091f503
2ung2rl <155498417+2ung2rl@users.noreply.github.com>
2024-02-19 18:29:41 +0900

    Delete src/twoweeks directory

--- src/twoweeks/Beauty.java
+++
@@ -1,7 +0,0 @@
-package twoweeks;
-
-public class Beauty extends Product {
-    public Beauty(String name, int price, double weight) {
-        super(name, price, weight);
-    }
-}
--- src/twoweeks/LargeAppliance.java
+++
@@ -1,7 +0,0 @@
-package twoweeks;
-
-public class LargeAppliance extends Product {
-    LargeAppliance(String name, int price, double weight) {
-        super(name, price, weight);
-    }
-}
